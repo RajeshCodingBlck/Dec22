8291f9bc233eb578c48930ccf20cd1b8cbcdb9ec
RajeshCodingBlck <137406636+RajeshCodingBlck@users.noreply.github.com>
2024-12-29 16:33:58 +0530

    Update CaseStudy.java

--- Lec_2/CaseStudy.java
+++ Lec_2/CaseStudy.java
@@ -4,7 +4,44 @@ public class CaseStudy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+//		 int a=12;
+//	        if(a==12){
+//	            System.out.println("a is 12");
+//	        }
+		// No a Error else part is optional.
+		
+		
+//		int a=12;
+//        if(a==12){
+//            int val2=13;
+//            System.out.println(a);
+//        }
+//        System.out.println(val2);
+		
+//		 val2 is not in if Block Scope. So give Compilation error.
+		
+//		Case Study :-3
+//		int a=12;
+//        if(a==12){
+//            int val2=13;
+//            System.out.println(a);
+//        }
+		
+//		 Scope chaining
+		
+//		Case Study -4
+//		int val2=12;
+//        int a=12;
+//        if(a==12){
+//            int val2=13;
+//            System.out.println(a);
+//        }
+		
+//		Same Name Backet never allowed.
+//		Like cpp it is allowed due to change in Scope.
+        
+		
+		
 	}
 
 }
