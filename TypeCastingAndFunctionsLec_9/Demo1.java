015420dbcf43c4ed2ae7bbd2ffc9f7f64dad50e3
RajeshCodingBlck <137406636+RajeshCodingBlck@users.noreply.github.com>
2025-01-27 14:08:46 +0530

    Add files via upload

---
+++ TypeCastingAndFunctionsLec_9/Demo1.java
@@ -0,0 +1,54 @@
+package TypeCastingAndFunctionsLec_9;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//      int a=-134;
+//      System.out.println(Integer.toBinaryString(a));
+      
+		// Implicite 
+//		int a=34;
+//		double d=a;
+//		System.out.println(d);
+		
+//		// Explicite
+//		double d=34;
+//		int a=(int)d;
+//		System.out.println(a);
+		
+		
+//		byte b=-34;
+//		int a=b;
+//		System.out.println(Integer.toBinaryString(a));
+		
+//		int a=512;
+//		byte b= (byte)a;
+//		System.out.println(b);
+		
+//		int a=13;
+//		long b=14;
+//		
+//		int ans=  (a+ b);
+		
+//		char ch='a';
+//		
+////		ch= (char)(ch+1);
+//		ch++;
+//		System.out.println(ch);
+		
+		
+		// literals
+		long a=100000000000000L;
+		System.out.println(a);
+		
+		float f=2.3f;
+		
+		
+		
+		
+		
+		
+	}
+
+}
