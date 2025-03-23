60132c4d02fef3112074c34bd0a286aea09af646
Rajesh <rajeshguptamc102@gmail.com>
2025-03-23 15:42:45 +0530

    WrapperClass

--- SortingAlgorithms/ArrayListDemo.java
+++ SortingAlgorithms/ArrayListDemo.java
@@ -5,6 +5,38 @@ public class ArrayListDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+//		int a=13;
+//		
+//		Integer a1=13;
+//		
+//		System.out.println(Integer.toBinaryString(13));
+//		
+//		System.out.println(Integer.MIN_VALUE);
+//		
+//		 String str= Integer.toString(145);
+//		 System.out.println(str);
+//		 
+//		 int b= Integer.parseInt(str);
+//		 System.out.println(b);
+		 
+		
+		Integer a=13;
+		int b=a;  // Unboxing
+		
+		System.out.println(b);
+		
+		int c=14;
+		Integer d=c;
+		
+		System.out.println(d);
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
