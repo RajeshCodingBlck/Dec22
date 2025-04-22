daa9661c5512a8134014c6d7363308f99cb3968c
Rajesh <rajeshguptamc102@gmail.com>
2025-04-22 21:09:50 +0530

    DoubtClass

--- DoubtClassAssingment2/shoppingGame.java
+++ DoubtClassAssingment2/shoppingGame.java
@@ -1,5 +1,48 @@
 package DoubtClassAssingment2;
 
 public class shoppingGame {
-
+   
+//	import java.util.*;
+//	public class Main {
+//
+//
+//	    public static void getwinner(int m, int n){
+//
+//
+//	        int turn=1;
+//	        int totalAayush=0;
+//	        int totalHarshit=0;
+//	        while(true){
+//
+//	             if(turn%2==0){ // Harshit
+//	               totalHarshit+= turn;
+//	               if(totalHarshit>n){
+//	                   System.out.println("Aayush");
+//	                   return;
+//	               }
+//	             }else { // Aayush
+//	                totalAayush+= turn;
+//	                if(totalAayush> m){
+//	                    System.out.println("Harshit");
+//	                    return;
+//	                }
+//	             }
+//
+//	             turn++;
+//	        }
+//
+//
+//	    }
+//	    public static void main(String args[]) {
+//
+//	       Scanner s= new Scanner(System.in);
+//	       int t= s.nextInt();
+//	      for(int i=1;i<=t;i++){
+//	          int m= s.nextInt(); // Aayush Limit
+//	          int n= s.nextInt();  // Harshit Limit
+//	          getwinner(m, n);
+//	      }
+//
+//	    }
+//	}
 }
